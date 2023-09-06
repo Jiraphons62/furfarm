3e10b26b22a2a7b6a34a8df99868dba5a94e9d6c
Jiraphons62 <Jiraphons62.nu.ac.th>
2023-09-07 00:19:06 +0700

    edit file

---
+++ front-end/js/frontend.js
@@ -0,0 +1,10 @@
+// document.getElementById("fetchDataButton").addEventListener("click", () => {
+//     fetch("/api/data")
+//         .then((response) => response.json())
+//         .then((data) => {
+//             document.getElementById("dataContainer").innerText = JSON.stringify(data);
+//         })
+//         .catch((error) => {
+//             console.error("เกิดข้อผิดพลาดในการดึงข้อมูล: ", error);
+//         });
+// });
